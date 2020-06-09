8d0254b29b6c8970aac75edc21d6577bae0f781c
Zhen <nil621@gmail.com>
2020-06-09 22:32:59 +1200

    add by script

--- docs/docs/docs/docs/docs/sync_r_intro_cn.sh
+++ docs/docs/docs/docs/docs/sync_r_intro_cn.sh
@@ -1,12 +1,16 @@
 # build gitbook and push to remote
-# gitbook published on my space
+# gitbook published 
 
+# generate gitbook
 gitbook build
 
-cp _book/* ./docs/
+# copy htmls to docs, then github pages use docs/ as its web resource
+cp -R _book/* ./docs/
 
+# add changes to stage
 git add .
 
+# commit
 git commit -m "add by script"
 
 # remote repo branch
